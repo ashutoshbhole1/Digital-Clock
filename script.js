e863c1e5ff4fd5da55dc8a7b7cf4a06c9bb80290
Ashutosh Bhole <ashutosh.kanhaashutech@gmail.com>
2025-03-27 00:31:46 +0530

    final

--- script.js
+++ script.js
@@ -4,9 +4,17 @@ let time = document.querySelector(".time");
 let date = document.querySelector(".date");
 let day = document.querySelector(".day");
 
+function updateClock() {
+    let d = new Date();  // so that everytime the d as a date object create so the interval will work 
+    time.innerHTML = d.toLocaleTimeString();
+    // date.innerHTML = d.toLocaleDateString();
+    date.innerHTML = `${d.getDate()}-${d.getMonth() + 1} -${d.getFullYear()} `;
+    day.innerHTML = d.toLocaleString('en-US', { weekday: 'long'});
+}
 
-let d = new Date();
-console.log(Date());
-console.log(d.getHours());
+setInterval(updateClock, 1000);
+updateClock();              // function call
+
+
 
 
