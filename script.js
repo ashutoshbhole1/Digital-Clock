ef37e6ec2cfeea3238cf7a4ade4a19817a4e24a3
Ashutosh Bhole <ashutosh.kanhaashutech@gmail.com>
2025-03-26 19:14:03 +0530

    rgta

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log("Radhe Radhe");
+
+let time = document.querySelector(".time");
+let date = document.querySelector(".date");
+let day = document.querySelector(".day");
+
+
+let d = new Date();
+console.log(Date());
+console.log(d.getHours());
+
+
